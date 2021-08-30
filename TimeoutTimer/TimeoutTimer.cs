e623211c7266c2e9af71b5d04f18c3ec919ed3ac
boysheo <boysheo@outlook.com>
2021-08-31 07:44:27 +0800

    1

---
+++ TimeoutTimer/TimeoutTimer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace susi.util.toolkit
+{
+    public readonly struct TimeoutTimer
+    {
+        public TimeoutTimer(TimeSpan timeout)
+        {
+            CreatTime = DateTimeOffset.UtcNow;
+            Timeout = timeout;
+        }
+
+        public TimeoutTimer(float timeoutSec) : this(TimeSpan.FromSeconds(timeoutSec))
+        {
+        }
+
+        public TimeSpan Timeout { get; }
+        public DateTimeOffset CreatTime { get; }
+
+        public void ThrowIfTimeout()
+        {
+            if (DateTimeOffset.UtcNow > CreatTime + Timeout)
+                throw new TimeoutException($"time out in {Timeout.TotalSeconds}s");
+        }
+    }
+}
